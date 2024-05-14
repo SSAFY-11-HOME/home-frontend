5e58ba975747e318e9c6c08493abe08a9028d0b0
김상현 <isayaksh@gmail.com>
2024-05-14 12:38:03 +0900

    <feat #1> 건물 매매 정보를 보관할 store 생성

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,11 +2,29 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
+  // 1. state
   const count = ref(0)
+
+  // 2. getter
   const doubleCount = computed(() => count.value * 2)
+
+  // 3. action
   function increment() {
     count.value++
   }
 
   return { count, doubleCount, increment }
+})
+
+export const useHouseStore = defineStore('house', () => {
+
+  const houses = ref([
+    {
+      name: "아파트1",
+      floor: "10층",
+      area: "10평",
+    },
+  ])
+
+  return { houses }
 })
