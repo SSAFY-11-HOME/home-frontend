dc66ec03c9b8947830d38cfcc36981c5b10a98b3
isayaksh <isayaksh@gmail.com>
2024-05-17 16:22:00 +0900

    <feat #3> KakaoMap.vue에 종속적인 카카오 지도 API를 store에 이전

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script setup>
-  import NavigationView from '@/views/NavigationView.vue';
-  import KakaoMap from '@/components/KakaoMap.vue';
+import NavigationView from '@/views/NavigationView.vue';
+import KakaoMap from '@/components/KakaoMap.vue';
+import { onMounted } from 'vue';
+
+import { useKakaoStore } from './stores/counter';
+
+const kakaoStore = useKakaoStore();
+
+onMounted(() => {
+	if(window.kakao && window.kakao.maps) {
+		kakaoStore.loadMap();
+	} else {
+		kakaoStore.loadScript();
+	}
+})
+
 </script>
 
 <template>
