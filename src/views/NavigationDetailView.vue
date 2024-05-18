aeadd751330e4f50ca94bbb7c53eb2f68fe31c36
isayaksh <isayaksh@gmail.com>
2024-05-18 13:47:28 +0900

    <refactor #3> 같은 페이지에서 파라미터 값만 변경될 경우, 페이지 갱신이 되지 않는 문제

--- src/views/NavigationDetailView.vue
+++ src/views/NavigationDetailView.vue
@@ -7,23 +7,11 @@ import NavigationDivision from '@/components/NavigationDivision.vue';
 import HouseDealListVue from '@/components/HouseDealList.vue';
 const route = useRoute();
 
-const houseInfo = ref({
-  aptCode: '11680000000023',
-  apartmentName: '경남아너스빌',
-  base: '서울특별시 강남구',
-  jibun: '역삼동 722',
-  road: '언주로85길 13-0',
-  dealList: [ 
-    {dealDate: '15.10.30', dealAmount: '61,000', area: '106.88', floor: '4', parea: '32.33'},
-    {dealDate: '18.01.22', dealAmount: '68,000', area: '96.54', floor: '3', parea: '29.2'},
-    {dealDate: '18.02.19', dealAmount: '69,800', area: '106.88', floor: '1', parea: '32.33'},
-    {dealDate: '19.09.30', dealAmount: '75,500', area: '96.54', floor: '3', parea: '29.2'},
-    {dealDate: '20.02.03', dealAmount: '81,000', area: '108.2', floor: '3', parea: '32.73'}
-  ]
-})
+const houseInfo = ref(null);
 
 selectAllById(route.params.id,
     ({data}) => {
+      console.log(data);
       houseInfo.value = data;
     },
     (error) => {
