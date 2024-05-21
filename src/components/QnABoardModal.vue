a606707278a2e1b2b5327b971076f1601be7f861
isayaksh <isayaksh@gmail.com>
2024-05-21 23:34:47 +0900

    <feat #12> Q&A 리스트 UI

--- src/components/QnABoardModal.vue
+++ src/components/QnABoardModal.vue
@@ -1,13 +1,9 @@
 <script setup>
 
 import { ref } from 'vue';
-import { useUserStore } from '@/stores/counter';
-import { join } from '@/api/user';
-
-const userStore = useUserStore();
 
 const modalCheck = ref(false);
-const pageCheck = ref(false);
+const writeCheck = ref(false);
 
 function openModal() {
 	modalCheck.value = !modalCheck.value;
@@ -24,12 +20,45 @@ function openModal() {
 
 		<div class="modal-container" @click="openModal">
 
-			<!-- 로그인 페이지 -->
-			<div id="header">
-				<img src="@/assets/logo.png" alt="logo" id="logo">
-				<div id="title">Q & A</div>
+			<!-- Q&A 목록 -->
+			<div v-if="!writeCheck">
+
+				<div id="header">
+					<img src="@/assets/logo.png" alt="logo" id="logo">
+					<div id="title">Q & A</div>
+				</div>
+
+				<div id="body">
+
+					<div class="content-head">
+						<div class="content-id">번호</div>
+						<div class="content-title">제목</div>
+						<div class="content-date">날짜</div>
+					</div>
+
+					<div class="content">
+						<div class="content-id">id</div>
+						<div class="content-title">title</div>
+						<div class="content-date">date</div>
+					</div>
+
+				</div>
+
 			</div>
 
+			<!-- 글쓰기 -->
+			<div v-if="writeCheck">
+
+				<div id="header">
+					<img src="@/assets/logo.png" alt="logo" id="logo">
+					<div id="title">글 쓰기</div>
+				</div>
+
+				<div id="body">
+
+				</div>
+
+			</div>
 
 			
 		</div>	
@@ -65,7 +94,6 @@ function openModal() {
   top: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.4);
   z-index: 2;
 
 }
@@ -73,10 +101,9 @@ function openModal() {
 /* modal or popup */
 .modal-container {
   position: fixed;
-  top: 120px;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 550px;
+  bottom: 64px;
+  right: 32px;
+  width: 600px;
   background: #fff;
   border-radius: 10px;
   padding: 20px;
@@ -87,83 +114,64 @@ function openModal() {
 }
 
 .modal-container #header {
-	width: 470px;
-
+	width: 520px;
 	display: flex;
-	justify-content: center;
-	align-items: center;
-
 	margin: 12px
+}
+
+.modal-container #body {
+
+	width: 560px;
+	height: 560px;
+
+	box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 }
 
 .modal-container #header #title {
 	font-size: 32px;
 	font-weight: bold;
 
-	color: #F7E600;
-	text-shadow: -1px 0 #3A1D1D, 0 1px #3A1D1D, 1px 0 #3A1D1D, 0 -1px #3A1D1D;
-}
-
-.input-box {
-	border: none; outline: none;
-	border-radius: 8px;
-	
-	background-color: #ebebeb;
-
-	width: 280px;
-
-	margin-bottom: 12px;
-
-	padding-left: 12px; padding-top: 4px; padding-bottom: 4px;
-
-	box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-}
-
-.check-box {
-	margin-bottom: 8px;
-}
-
-.check-box * {
-	margin: 4px;
-}
-
-#login-button {
-
-	width: 280px;
-
-	border: none;
-	border-radius: 8px;
-
-	background-color: #F7E600;
-
 	color: #3A1D1D;
-	font-size: 18px;
-	font-weight: bold;
-
-	box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+	
 }
 
-#join-tab {
+#body .content-head {
+	width: 560px;
 	display: flex;
-	justify-content: center;
-	align-items: center;
-	
-
-	width: 510px;
-	height: 48px;
-
-	line-height: 48px;
 }
 
-#join-tab *{
-	margin: 8px;
-	color: #747474;
-}
-
-#join-button {
-	color: #3A1D1D;
+#body .content-head .content-id {
 	font-weight: bold;
-	cursor: pointer;
+}
+
+#body .content-head .content-title {
+	font-weight: bold;
+}
+
+#body .content-head .content-date {
+	font-weight: bold;
+}
+
+.content-id {
+	width: 140px;
+	margin: 12px;
+}
+
+.content-title {
+	width: 320px;
+	margin: 12px;
+}
+
+.content-date {
+	width: 140px;
+	margin: 12px;
+}
+
+#body .content {
+	/* border: solid black 1px; */
+
+	width: 560px;
+	display: flex;
 }
 
 #logo {
